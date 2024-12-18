6810f1b28ef9a0b53011ff183938fa5e6e1c42bf
EstebanGomezVal <157132736+EstebanGomezVal@users.noreply.github.com>
2024-12-17 23:39:42 -0600

    agregar titanic

--- Pyhton/app.py
+++ Pyhton/app.py
@@ -7,9 +7,18 @@ def main():
     st.text('Esto es un texto')
     nombre = 'Esteban'
     st.text(f'Hola {nombre} esto es un texto')
-    st.markdown('# Esto es un markdown')
     st.markdown('## Esto es un markdown')
-    st.markdown('### Esto es un markdown')
+    
+    st.success('Exito')
+    st.warning('Advertencia')
+    st.info('informacion')
+    st.error('Error')
+
+    st.write('Esta es la suma de uno mas dos')
+    st.write(1+2)
+
+
+
 
 
 if __name__ == '__main__':
