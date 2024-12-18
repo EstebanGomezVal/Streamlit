961b2d309d5b7ff0e8046b99676d6a2b12fcb8ea
EstebanGomezVal <157132736+EstebanGomezVal@users.noreply.github.com>
2024-12-17 23:04:23 -0600

    streamlit

--- Pyhton/app.py
+++ Pyhton/app.py
@@ -2,6 +2,15 @@ import streamlit as st
 
 def main():
     st.title('Curso Streamlit')
+    st.header('Esto es un encabezado')
+    st.subheader('Esto es un sub encabezado')
+    st.text('Esto es un texto')
+    nombre = 'Esteban'
+    st.text(f'Hola {nombre} esto es un texto')
+    st.markdown('# Esto es un markdown')
+    st.markdown('## Esto es un markdown')
+    st.markdown('### Esto es un markdown')
+
 
 if __name__ == '__main__':
     main()
