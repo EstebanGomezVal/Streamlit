7f85c3939c0aa5b136d2e11eeb567d7dddf75437
EstebanGomezVal <157132736+EstebanGomezVal@users.noreply.github.com>
2024-12-18 00:58:07 -0600

    videos

--- Pyhton/Videos.py
+++ Pyhton/Videos.py
@@ -3,9 +3,9 @@ from PIL import Image
 
 def main():
     st.title('Videos')
-    st.header('Imagen de escritorio:')
-    img = Image.open('Lluvia.webp')
-    st.image(img, use_column_width=True)
+    # st.header('Imagen de escritorio:')
+    # img = Image.open('Lluvia.webp')
+    # st.image(img, use_column_width=True)
 
     st.header('Imagen de internet:')
     st.image('https://i.pinimg.com/originals/2b/ea/b0/2beab067ba0e6076643c8f70fb8d884b.jpg')
@@ -14,7 +14,9 @@ def main():
     with open('Lluvia.mp4', 'rb') as video_file:
         st.video(video_file.read(), start_time=0)
 
-
+    # with open('Cancion.mp3', 'rb') as audio_file:
+        # st.audio(audio_file.read())'''
+    
 
 
 
